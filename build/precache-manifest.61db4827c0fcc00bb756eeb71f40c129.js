40d5f1f4b8863c4ede7560dbde2b2ef98c2419f1
Matt Lodge <mattpodge@gmail.com>
2020-01-16 15:17:24 +0000

    Deploy the build and see if we can get it on github pages

---
+++ build/precache-manifest.61db4827c0fcc00bb756eeb71f40c129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59635688e0a8ab90962b5f47474d97a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccb134f243065f533fcb",
+    "url": "/static/css/main.72f466a8.chunk.css"
+  },
+  {
+    "revision": "236a3451ec4fe9bc0617",
+    "url": "/static/js/2.383282f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.383282f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccb134f243065f533fcb",
+    "url": "/static/js/main.45cd7cb0.chunk.js"
+  },
+  {
+    "revision": "16b1a87c8bfdb3b0d98f",
+    "url": "/static/js/runtime-main.d68248f3.js"
+  }
+]);
