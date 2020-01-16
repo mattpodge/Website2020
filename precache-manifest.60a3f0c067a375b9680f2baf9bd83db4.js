163c2c039b3fcc901274a2f06d49eb1eea439f56
Matt Lodge <mattpodge@gmail.com>
2020-01-16 15:25:26 +0000

    Updates

---
+++ precache-manifest.60a3f0c067a375b9680f2baf9bd83db4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d904356baf8768924db44fa92c2928",
+    "url": "/selena/index.html"
+  },
+  {
+    "revision": "ccb134f243065f533fcb",
+    "url": "/selena/static/css/main.72f466a8.chunk.css"
+  },
+  {
+    "revision": "236a3451ec4fe9bc0617",
+    "url": "/selena/static/js/2.383282f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/selena/static/js/2.383282f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccb134f243065f533fcb",
+    "url": "/selena/static/js/main.45cd7cb0.chunk.js"
+  },
+  {
+    "revision": "e369f3ee4efb1f610dac",
+    "url": "/selena/static/js/runtime-main.f5537d0a.js"
+  }
+]);
