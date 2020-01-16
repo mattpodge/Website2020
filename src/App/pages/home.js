6db8d5a5b3801ecb5093d7f67a7604a5c4b98b1c
Matt Lodge <mattpodge@gmail.com>
2020-01-16 15:07:57 +0000

    Built a basic website, very basic

---
+++ src/App/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return(
+        <div class='title'>
+            <h1>Matthew Lodge</h1>
+            <p>User Interface &amp; Experience Designer</p>
+        </div>
+    )
+}
+
+export default Home;
