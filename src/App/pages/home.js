1fa480835488ae3d31e3c9e48aa4462599b0a972
Matt Lodge <mattpodge@gmail.com>
2020-01-23 11:07:49 +0000

    PLaying around with basic fluid typography and added social links

--- src/App/pages/home.js
+++ src/App/pages/home.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 function Home() {
     return(
-        <div class='title'>
-            <h1>Matthew Lodge</h1>
-            <p>User Interface &amp; Experience Designer</p>
-        </div>
+        <React.Fragment>
+            <div className='title'>
+                <h1>Matthew Lodge</h1>
+                <p>User Interface &amp; Experience Designer</p>
+            </div>
+
+            <div className='socials'>
+                <a className='socials__link' href='mailto:mattpodge@gmail.com' title='Send me a friendly email'>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                </a>
+                <a className='socials__link' href='https://www.linkedin.com/in/matthew-lodge-90753ba1/' title='Visit my LinkedIn profile'>
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+            </div>
+        </React.Fragment>
     )
 }
 
