5defecf1f45b4e79d248100c1cf001692039ac61
Matt Lodge <mattpodge@gmail.com>
2020-04-01 10:31:02 +0100

    Created a basic button component and a scratchpad page in order to try out styling

---
+++ src/App/pages/scratchpad.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Scratchpad() {
+    return(
+        <React.Fragment>
+            Hello world!
+        </React.Fragment>
+    )
+}
+
+export default Scratchpad;
