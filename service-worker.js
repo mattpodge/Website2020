ca66da92f5485fb7b3a2aa7aa25108259c8c467a
Matt Lodge <mattpodge@gmail.com>
2020-08-20 12:05:20 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f85cf14e1487bd47e4eb075b13740cab.js"
+  "/precache-manifest.f59ec94bba45f86ea192522e0b4b3ace.js"
 );
 
 self.addEventListener('message', (event) => {
