f1b85a7b4f2f204d465106180edb289fe74f5ce3
Matt Lodge <mattpodge@gmail.com>
2020-08-21 13:13:22 +0100

    Updates

--- precache-manifest.785ad69dc58c043bc2d3b626f991100a.js
+++ precache-manifest.785ad69dc58c043bc2d3b626f991100a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5419df0c0898948ad18ece373fca6f7a",
+    "revision": "addb528d6aca659875b6c67d43a67791",
     "url": "/index.html"
   },
   {
-    "revision": "069c11e8ccbf0ab40dad",
-    "url": "/static/css/main.0cc8882d.chunk.css"
+    "revision": "c3975117efbbf2221274",
+    "url": "/static/css/main.f5b15b5c.chunk.css"
   },
   {
     "revision": "951ae3ed78f39007d7d1",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.9d177475.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "069c11e8ccbf0ab40dad",
-    "url": "/static/js/main.561554d1.chunk.js"
+    "revision": "c3975117efbbf2221274",
+    "url": "/static/js/main.4e0b0a6c.chunk.js"
   },
   {
     "revision": "e799a4e24fdd0bec77a6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.659879044bca24797edcb1716b2536e8.js"
+  "/precache-manifest.785ad69dc58c043bc2d3b626f991100a.js"
 );
 
 self.addEventListener('message', (event) => {
