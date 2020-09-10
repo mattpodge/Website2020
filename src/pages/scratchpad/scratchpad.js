eba26ef3447ee887abd07c9b3a6bcf1a922d9cc6
mattpodge <mattpodge@gmail.com>
2020-09-10 11:33:37 +0100

    Getting some typography styling sorted

--- src/pages/scratchpad/scratchpad.js
+++ src/pages/scratchpad/scratchpad.js
@@ -18,6 +18,21 @@ function Scratchpad() {
     return(
         <main className='content'>
             <h1>Scratchpad</h1>
+            <Example title='Headings Typography'>
+                <h1>The Quick Brown Fox Jumps Over The Lazy Dog</h1>
+                <h2>The Quick Brown Fox Jumps Over The Lazy Dog</h2>
+                <h3>The Quick Brown Fox Jumps Over The Lazy Dog</h3>
+                <h4>The Quick Brown Fox Jumps Over The Lazy Dog</h4>
+                <h5>The Quick Brown Fox Jumps Over The Lazy Dog</h5>
+                <h6>The Quick Brown Fox Jumps Over The Lazy Dog</h6>
+            </Example>
+            <Example title='Body Copy Typography'>
+                <h1>The Quick Brown Fox Jumps Over The Lazy Dog</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem iusto, laborum autem officiis aliquid totam praesentium. Reprehenderit deleniti error nemo autem ipsa blanditiis a, voluptatem, veritatis exercitationem quibusdam cupiditate quod!</p>
+                <p>At dicta facilis deserunt eaque commodi ab expedita et mollitia, ut alias facere maiores minima nam. Labore delectus perspiciatis in reprehenderit, autem quis sapiente beatae suscipit praesentium accusantium nulla earum.</p>
+                <h2>The Quick Brown Fox Jumps Over The Lazy Dog</h2>
+                <p>Assumenda numquam accusantium maiores laudantium reprehenderit molestiae. Quisquam sunt eius, beatae magni, quam dolor consectetur tempore maxime sint, voluptate obcaecati itaque soluta. Eligendi repudiandae rerum nam culpa ad, sint minima!</p>
+            </Example>
             <Example title='Button Components'>
                 <Button href='https://www.google.com'>Button Test</Button>
             </Example>
