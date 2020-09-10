439bd29597188f07271ca2d9be9e248ae0676d0b
mattpodge <mattpodge@gmail.com>
2020-09-10 12:34:22 +0100

    Making some styling choices for typography in to variables

--- src/pages/scratchpad/scratchpad.js
+++ src/pages/scratchpad/scratchpad.js
@@ -32,6 +32,15 @@ function Scratchpad() {
                 <p>At dicta facilis deserunt eaque commodi ab expedita et mollitia, ut alias facere maiores minima nam. Labore delectus perspiciatis in reprehenderit, autem quis sapiente beatae suscipit praesentium accusantium nulla earum.</p>
                 <h2>The Quick Brown Fox Jumps Over The Lazy Dog</h2>
                 <p>Assumenda numquam accusantium maiores laudantium reprehenderit molestiae. Quisquam sunt eius, beatae magni, quam dolor consectetur tempore maxime sint, voluptate obcaecati itaque soluta. Eligendi repudiandae rerum nam culpa ad, sint minima!</p>
+                <h3>The Quick Brown Fox Jumps Over The Lazy Dog</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus officiis quos culpa dignissimos consequatur nostrum asperiores temporibus eveniet provident minima?</p>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Quam excepturi ex pariatur doloremque.</li>
+                    <li>Corrupti alias iste veritatis possimus!</li>
+                    <li>Quaerat, voluptate dolorum! Sequi, voluptatem!</li>
+                </ul>
+                <p>Commodi assumenda unde, deserunt voluptatibus in laudantium eius eaque rem ratione doloremque iste accusantium praesentium aut deleniti temporibus earum aliquam.</p>
             </Example>
             <Example title='Button Components'>
                 <Button href='https://www.google.com'>Button Test</Button>
