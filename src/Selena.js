b1b9778ad6016ddbe5373d2c787165929daacf8f
mattpodge <mattpodge@gmail.com>
2020-10-13 12:35:01 +0100

    Scroll to top added. Taken wholesale from the reactrouter instructions

--- src/Selena.js
+++ src/Selena.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Header } from './components/header';
+import ScrollToTop from './components/scrolltotop/scrolltotop';
 import { Home, Work } from './pages';
 
 function Selena() {
   return (
     <Router>
-        <Header></Header>
+        <ScrollToTop />
+        <Header />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/work/:slug' component={Work} />
