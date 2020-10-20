9f72423c5564f258b8b1c065ff63c04f0cb8113a
mattpodge <mattpodge@gmail.com>
2020-10-20 12:53:39 +0100

    Written up my about page and added a photo

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,31 +1,32 @@
 import React from 'react';
 
+import image from '../../imgs/ben-nevis-shot.png';
 import styles from './about.module.scss';
 
 function About(props) {
     return(
         <React.Fragment>
+            <div className={styles.heading}>
+                <h1>Hello, I'm <span>Matthew Lodge</span></h1>
+            </div>
             <main className={styles.content}>
-                <h1>About Me</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, quo vel temporibus ad eos enim quas veritatis natus dolor voluptate possimus eveniet est, ratione aliquid illo. Blanditiis dolores aliquam enim!</p>
-                <p>Error asperiores debitis quisquam laudantium eos provident nostrum in! Ad, aliquam sed. Maiores consequatur at non deserunt, obcaecati doloribus itaque sequi, fuga ex quis ducimus quisquam quam quibusdam, earum tempore.</p>
-                <p>Consequuntur cupiditate placeat itaque tempore et, reiciendis sint. Necessitatibus odio odit accusamus ipsa tempore exercitationem, placeat commodi quasi ea reprehenderit nemo nobis aut optio voluptatibus aliquam ex sequi. Velit, iste.</p>
+                <p>And for the last 5(ish) years I've been a <span>User Interface Designer</span> (with some UX work here &amp; there) in the software industry. Prior to that I spent 10+ years as a <span>Web &amp; Graphic Designer</span> working on everything from brand design to marketing material.</p>
+                <p>I enjoy music any time of day, playing video games and watching movies (once the work is done), and hiking holidays with my girlfriend.</p>
+                <p>I'm am currently on the search for a <span>permanent UI Designer</span> role.</p>
             </main>
             <aside className={styles.aside}>
-                <h6>Recently Playing</h6>
-                <ul>
-                    <li>Game</li>
-                    <li>Game</li>
-                    <li>Game</li>
-                </ul>
-                <h6>On Rotation</h6>
-                <ul>
-                    <li>Album - Artist</li>
-                    <li>Album - Artist</li>
-                    <li>Album - Artist</li>
-                </ul>
+                <h6>On Rotation...</h6>
+                <dl>
+                    <dt>Mouth Dreams</dt>
+                    <dd>Niel Cicierega</dd>
+                </dl>
+                <h6>Playing...</h6>
+                <dl>
+                    <dt>Hades</dt>
+                    <dd>Supergiant Games</dd>
+                </dl>
                 <h6>Here's Me</h6>
-                <p>Pic</p>
+                <img className={styles.headshot} src={image} alt="Matthew Lodge sat at the peak of Ben Nevis, enjoying a glass of Scotch" />
             </aside>
         </React.Fragment>
     )
